e7a70592d924e863d0b996a4d6d908b63e3eff68
diasaleh <dia-20111@hotmail.com>
2017-11-28 11:46:09 +0200

    new neg

--- NegWordsFreq/main.py
+++ NegWordsFreq/main.py
@@ -5,10 +5,10 @@ import xlsxwriter
 from collections import Counter
 from collections import defaultdict
 
-size=1
+size=74
 book = xlrd.open_workbook('/Users/diasaleh/Desktop/GP/neg.xlsx')
 sheet = book.sheet_by_name('Sheet1')
-workbook = xlsxwriter.Workbook('/Users/diasaleh/Desktop/1000allNegCatNew.xlsx')
+workbook = xlsxwriter.Workbook('/Users/diasaleh/Desktop/100allNegCatNew.xlsx')
 worksheet = workbook.add_worksheet()
 format = workbook.add_format()
 format.set_bold()
@@ -21,6 +21,7 @@ print sheet.ncols
 col=1
 roww=1
 j=0
+negCount=[0]*sheet.ncols
 for i in range(0,sheet.ncols):
     for row in range(0, sheet.nrows):
         if sheet.cell_value(row, i) != "":
@@ -29,7 +30,7 @@ for i in range(0,sheet.ncols):
     j=0
 print neg
 for i in range(1,size+1):
-    f = open("/Users/diasaleh/Desktop/new/100/n (74).txt", "r")
+    f = open("/Users/diasaleh/Desktop/new/100/t ("+str(i)+").txt", "r")
     print "/Users/diasaleh/Desktop/GP/100/TN"+str(i)+".txt"
     sentence = f.read()
     x = sentence.split()
@@ -42,7 +43,7 @@ for i in range(1,size+1):
     counts = Counter(x)
 
     # print words
-    # print neg[0][0]
+    print neg[0][0]
     for j in range(0,sheet.ncols):
         for p in range(0,len(neg[j])):
                 # print neg[j][p]
@@ -54,16 +55,20 @@ for i in range(1,size+1):
 
     for j in range(0,sheet.ncols):
         worksheet.write(roww, col, str(i) +"  " +neg[j][0], format)
-        worksheet.write(roww + 1, col, 100*negCount[j]/sum(negCount), format)
-        roww +=3
+        worksheet.write(roww+1, col, negCount[j], format)
+        if sum(negCount) !=0:
+            worksheet.write(roww + 2, col, 100*negCount[j]/sum(negCount), format)
+        roww +=4
+    print negCount
+
+    negCount = [0] * sheet.ncols
 
     roww = 1
     col +=1
-    print negCount
 
-roww = 3
+roww = 4
 for j in range(0, sheet.ncols):
     for b in range(1,75):
         worksheet.write(roww, b, 100*avgNegCount[j]/sum(avgNegCount), format)
-    roww += 3
+    roww += 4
 workbook.close()
