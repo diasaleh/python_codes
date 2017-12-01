a813a54b3c9f9f4b4e9c95d1b2a8d0a78135cb3b
diasaleh <dia-20111@hotmail.com>
2017-12-01 16:39:48 +0200

    new

--- charOut/main.py
+++ charOut/main.py
@@ -14,7 +14,11 @@ size=int(sys.argv[3])+1
 import collections
 avgcat1=0
 avgcat2=0
-workbook = xlsxwriter.Workbook('/Users/diasaleh/Desktop/'+str(sys.argv[4])+'all_Mjhor_Mhmos_details.xlsx')
+avgcat3=0
+avgcat4=0
+avgcat5=0
+
+workbook = xlsxwriter.Workbook('/Users/diasaleh/Desktop/'+str(sys.argv[4])+'all_charOut.xlsx')
 worksheet = workbook.add_worksheet()
 format = workbook.add_format()
 format.set_bold()
@@ -50,33 +54,44 @@ for i in range(1,size):
     cat1=0
     cat2=0
     cat3=0
+    cat4 = 0
+    cat5 = 0
     for k, v in od.items():
         #print "%s : %d" % (k,v)
         sentence = unicode(k, "utf-8")
-        # مجهورة ( Voiced )(ب، ج، د، ر، ز، ط، ع، غ، ص، ل، م، ن، و، ي)
-        if(k =="ع"or k=="غ" or k == "ق"or k=="ج"  or k=="ب"  or k=="ض"  or k=="ل"  or k=="ن"  or k=="ر"   or k=="د"  or k=="ز"  or k=="ذ"  or k=="ظ"  or k=="و"  or k=="م"  or k=="ي" ):
+        if(k =="ء"or k=="أ" or k=="إ"  or k=="ئ"  or k=="ؤ"  or k=="ه"  or k=="ع"  or k=="غ"   or k=="ح"  or k=="خ"   ):
             cat1+= float((v*100)/(n* 1.0))
-
-        # مهموسة ( Voiceless ) حروفها : (ف، ح، ث، هـ، ش، خ، ص، س، ك، ت، ق، ط، ء) - عند المحدثين
-        elif k == "ت" or k == "س" or k == "ش" or k == "ك" or k == "خ" or k == "ه" or k == "ح" or k == "ص" or k == "ث"  or k ==  "ف" :
+        elif k == "ق" or k == "ك" or k == "ج" or k == "ش" or k == "ي" or k == "ت" or k == "ط" or k == "د" or k == "ث" or k == "ظ" or k == "ذ"or k=="ن" or k == "ر" or k == "ز" or k == "ص"or k=="س"  or k == "ض"or k=="ل":
             cat2 += float((v * 100) / (n * 1.0))
-        else:
-            print v, k
+        elif k == "ب" or k == "م" or k == "و" or k == "ف":
             cat3 += float((v * 100) / (n * 1.0))
+        elif k == "و" or k == "ي" or k == "ا" or k == "ى":
+            cat4 += float((v * 100) / (n * 1.0))
+        else:
+            cat5 += float((v * 100) / (n * 1.0))
     avgcat1 += cat1
     avgcat2+=cat2
+    avgcat3+=cat3
+    avgcat4+=cat4
+    avgcat5+=cat5
     print i
-    print "\nMjhor: " + str(cat1)
-    print "\nMhmos: " + str(cat2)
+    print "\nhulk: " + str(cat1)
+    print "\nlesan: " + str(cat2)
+    print "\nshefa: " + str(cat3)
+    print "\njuff: " + str(cat4)
     print "\n=============\n"
-    worksheet.write(row, col, str(i) + " Mjhor",format)
+    worksheet.write(row, col, str(i) + " hulk",format)
     worksheet.write(row+1, col, cat1,format)
 
-    print cat3
+    worksheet.write(row+3, col, str(i) + " lesan", format)
+    worksheet.write(row + 4, col, cat2, format)
 
-    worksheet.write(row+3, col, str(i) +" Mhmos",format)
-    worksheet.write(row + 4, col , cat2,format)
-    worksheet.write(row+6, col, cat3,format)
+    worksheet.write(row+6, col, str(i) + " shefa", format)
+    worksheet.write(row + 7, col, cat3, format)
+
+    worksheet.write(row+9, col, str(i) +" juff",format)
+    worksheet.write(row + 10, col , cat4,format)
+
     col += 1
     row=3
     # for k, v in od2.items():
@@ -87,9 +102,12 @@ for i in range(1,size):
     #     worksheet.write(row, col, sentence)
     #     worksheet.write(row, col + 1, float((v*100)/(n* 1.0)), format)
     #     row+=1
-size = size-1
+size=size-1
+row = 3
 for k in range(0,size+1):
-    worksheet.write(5, k, avgcat1/size,format)
-    worksheet.write(8, k, avgcat2/size,format)
+    worksheet.write(row+2, k, avgcat1/size,format)
+    worksheet.write(row+5, k, avgcat2/size,format)
+    worksheet.write(row+8, k, avgcat3/size,format)
+    worksheet.write(row+11, k, avgcat4/size,format)
 
 workbook.close()
